d46ff9da0c2cf29bf23bb60bba9bb822100bdf65
mmufid298 <96731433+mmufid298@users.noreply.github.com>
2022-03-09 12:26:30 +0700

    Section 8

---
+++ 5.js
@@ -0,0 +1,5 @@
+let value = [1,2,3,4,5,6];
+let value1 = value.map((data) => {
+  return data * 2;
+});
+console.log(value1);
---
+++ 6.js
@@ -0,0 +1,15 @@
+let a1 = 1.5;
+let b1 = Math.round(a1);
+
+		
+let a2 = 2.56;
+let b2 = Math.round(a2);
+
+		
+let a3 = 5.1;
+let b3 = Math.round(a3);
+
+let a4 = 12.33;
+let b4 = Math.round(a4);
+
+console.log(b1, b2, b3, b4); 
