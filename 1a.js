65707afc1e18a8cdb63e837000b82a3d3d63ef95
mmufid298 <96731433+mmufid298@users.noreply.github.com>
2022-03-09 12:29:00 +0700

    Section 8

--- 1a.js
+++ 1a.js
@@ -3,5 +3,6 @@ let b = "kampus merdeka";
 const nama = "Budi";
 let terdaftar = false;
 let lengkap_arr = [a, b, nama, terdaftar];
+let getlengkap_arr = lengkap_arr[2];
 
-console.log(nama);
+console.log(getlengkap_arr);
